bf2f3c6869dc259b3d62d796ccee8dd466451c85
TDK74 <98406593+TDK74@users.noreply.github.com>
2025-01-10 14:33:01 +0200

    Create Ch1_01.py
    Added new exercise Python file:
    Ch1_01.py

---
+++ Ch1_01.py
@@ -0,0 +1,16 @@
+# you can define your own functions
+#def greetings(name, catch_phrase):
+#    print("Welcome " + name + catch_phrase + "!")
+
+def greet(catch_phrase):
+    print("Welcome " + catch_phrase + "!")
+
+# go ahead and enter your own name in variable 'name' below to get a warm welcome from us.
+
+def main():
+    name = "Tsvetan "
+    catch_phrase = "to Building AI"
+#    greetings(name, catch_phrase)
+    greet(catch_phrase)
+
+main()
